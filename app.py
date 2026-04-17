f6f4a19d1ce1a1926fcce17a3d6f88a52f27d967
santoshml-lab <santoshkrsbg36@gmail.com>
2026-04-17 18:09:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,3 +50,26 @@ if submit:
     pred = model.predict(df)[0]
 
     st.success(f"Prediction: {pred}")
+    import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+
+st.subheader("📊 Feature Importance Chart")
+
+# importance extract
+importances = model.feature_importances_
+
+# dataframe
+feat_df = pd.DataFrame({
+    "Feature": columns,
+    "Importance": importances
+})
+
+# sort
+feat_df = feat_df.sort_values(by="Importance", ascending=True)
+
+# plot
+fig, ax = plt.subplots()
+ax.barh(feat_df["Feature"], feat_df["Importance"])
+
+st.pyplot(fig)
